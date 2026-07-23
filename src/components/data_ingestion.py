75e15cf1c548677a55616373196284261a29dc86
printf-sourav <lcs.souravkrsahu@gmail.com>
2026-07-23 17:11:57 +0530

    Hyper parameter tuning done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -48,9 +50,11 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     
-    data_transformation.initiate_data_transformation(
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(
             train_data,
             test_data
     )    
+    modeltrainer=ModelTrainer()
+    best_score=modeltrainer.initiate_model_trainer(train_arr,test_arr)
     
 
--- src/utils.py
+++ src/utils.py
@@ -3,9 +3,10 @@ import sys
 
 import pandas as pd
 import numpy as np
-
+from sklearn.metrics import (r2_score)
 from src.exception import CustomException
 import dill 
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path,obj):
@@ -19,3 +20,32 @@ def save_object(file_path,obj):
     
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            para = params[model_name]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)  
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_predict = model.predict(X_train)
+            y_test_predict = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_predict)
+            test_model_score = r2_score(y_test, y_test_predict)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
